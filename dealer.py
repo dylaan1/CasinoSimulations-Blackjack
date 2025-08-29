9c8cff0a4b5c3dad3c07b03f48ad6c60eb223ef8
dylaan1 <163621865+dylaan1@users.noreply.github.com>
2025-08-29 12:22:33 -0700

    Update dealer.py

--- dealer.py
+++ dealer.py
@@ -1,9 +1,9 @@
  (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
 diff --git a//dev/null b/blackjack/dealer.py
-index 0000000000000000000000000000000000000000..ba8cc20622c37d15e035321e717eb2b3289f22bb 100644
+index 0000000000000000000000000000000000000000..2c734055c4597eef825911021c6aa847bed10d81 100644
 --- a//dev/null
 +++ b/blackjack/dealer.py
-@@ -0,0 +1,21 @@
+@@ -0,0 +1,18 @@
 +from __future__ import annotations
 +from dataclasses import dataclass
 +from .cards import Shoe
@@ -13,13 +13,10 @@ index 0000000000000000000000000000000000000000..ba8cc20622c37d15e035321e717eb2b3
 +class Dealer:
 +    hit_soft_17: bool = False
 +
-+    def play(self, shoe: Shoe) -> Hand:
-+        hand = Hand()
-+        hand.add_card(shoe.draw())
-+        hand.add_card(shoe.draw())
++    def play(self, hand: Hand, shoe: Shoe) -> Hand:
 +        while True:
 +            value = hand.best_value
-+            soft = len(hand.values) > 1 and value != min(hand.values)
++            soft = any(v <= 21 and v != value for v in hand.values)
 +            if value < 17 or (value == 17 and soft and self.hit_soft_17):
 +                hand.add_card(shoe.draw())
 +                continue
