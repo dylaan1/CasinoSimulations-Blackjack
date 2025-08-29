b0ab0537c50fbb79dde4874c536cf2b077719980
dylaan1 <163621865+dylaan1@users.noreply.github.com>
2025-08-29 12:26:00 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
  (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
 diff --git a//dev/null b/blackjack/settings.py
-index 0000000000000000000000000000000000000000..710016b8ebca5877f4fb14cf4f4e570c326ac6f7 100644
+index 0000000000000000000000000000000000000000..48dc6723d570a6291ff2f6e07efc541de821978d 100644
 --- a//dev/null
 +++ b/blackjack/settings.py
-@@ -0,0 +1,15 @@
+@@ -0,0 +1,16 @@
 +from dataclasses import dataclass
 +
 +@dataclass
@@ -14,6 +14,7 @@ index 0000000000000000000000000000000000000000..710016b8ebca5877f4fb14cf4f4e570c
 +    blackjack_payout: float = 1.5
 +    double_after_split: bool = True
 +    resplit_aces: bool = False
++    bet_amount: float = 1.0
 +    num_decks: int = 6
 +    hit_soft_17: bool = False
 +    penetration: float = 0.75
